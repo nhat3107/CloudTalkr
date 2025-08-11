c0b7a716c3d0d721fb051be43e1878eb5a749283
Luong Minh Nhat <nhatluong.it@gmail.com>
2025-08-11 17:48:45 +0700

    update deploy backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.NODE_ENV === "development"
+  import.meta.env.VITE_NODE_ENV === "development"
     ? "http://localhost:5001/api"
     : import.meta.env.VITE_BACKEND_URL + "/api";
 
