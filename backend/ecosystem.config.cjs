07f6011894e9c13b606c34193d4e338b1590e992
Luong Minh Nhat <nhatluong.it@gmail.com>
2025-08-13 02:40:57 +0700

    update

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,16 +1,12 @@
-export default {
+module.exports = {
   apps: [
     {
       name: "backend-app",
       script: "npm",
       args: "run start",
-      interpreter: "none",
       instances: 1,
       env: {
         NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
       },
     },
   ],
