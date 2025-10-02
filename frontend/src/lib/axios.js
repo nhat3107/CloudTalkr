c0e494e66e7ae3df002d2119281e37e3cbba123a
Luong Minh Nhat <nhatluong.it@gmail.com>
2025-10-02 10:58:31 +0700

    Config: Update docker to deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL + "/api";
+const BASE_URL = import.meta.env.NODE_ENV === "production" ? import.meta.env.VITE_BACKEND_URL + "/api" : "http://localhost:3000/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
